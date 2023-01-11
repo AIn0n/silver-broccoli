5132520dfb645f65c6c441a4afdb73566ff678a7
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-11 22:31:10 +0100

    Room view done

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 const route = useRoute();
+const router = useRouter();
 const name = route.params.name;
 </script>
 
 <template lang="pug">
-p {{ name }}
+div(class="container text-center")
+  h1(class="my-5") {{ name }}
+  div(class="alert alert-primary") placeholder for chart
+button(@click="router.push('/')" class="btn btn-outline-secondary position-absolute bottom-0 end-0 mx-5 my-5 fs-4") back
 </template>
