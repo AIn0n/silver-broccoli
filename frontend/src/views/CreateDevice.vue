81bbd726181e612edc7c8d8071dc8da1913dfc7c
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-12 14:44:50 +0100

    minor refactoring of device creation view

--- frontend/src/views/CreateDevice.vue
+++ frontend/src/views/CreateDevice.vue
@@ -8,15 +8,22 @@ const device_types = ["default", "solar", "accumulator"];
 <template lang="pug">
 div(class="container w-50 text-center")
   h1(class="my-5") Add new device
-  p(class="fs-5 mt-3") Device name
+  fa-icon(icon="fa-house-laptop" class="me-3 fa-lg")
+  span(class="fs-5") Device name
   input(class="form-control form-control-lg" type="text" placeholder="new device's name")
-  p(class="fs-5 mt-3") type
+  div(class="container mt-3")
+    fa-icon(icon="fa-lightbulb" class="me-3 fa-lg")
+    span(class="fs-5") type
   select(class="form-select form-select-lg")
     option(v-for="device_type in device_types") {{ device_type }}
-  p(class="fs-5 mt-3") energy class
+  div(class="container mt-3")
+    fa-icon(icon="fa-plug" class="me-3 fa-lg")
+    span(class="fs-5 mt-3") energy class
   select(class="form-select form-select-lg")
     option(v-for="cls in energy_classes") {{ cls }}
-  p(class="fs-5 mt-3") energy drain
+  div(class="container mt-3")
+    fa-icon(icon="fa-bolt" class="me-3 fa-lg")
+    span(class="fs-5 mt-3") energy drain
   div(class="input-group")
     input(type="text" class="form-control form-control-lg" placeholder="energy drain in kWh")
     span(class="input-group-text") kWh
