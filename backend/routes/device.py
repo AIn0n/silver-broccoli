90425037037acc609463efb7834f8cf027f044fc
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-14 01:18:11 +0100

    added helper entry points for device (type and energy class)

---
+++ backend/routes/device.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from models.device import Device, EnergyClass, DeviceType
+from config.db import conn
+
+device = APIRouter()
+
+
+@device.post("/{room}/device")
+def add_new_device(room: str, device: Device):
+    conn["database"]["rooms"].update_one(
+        {"name": room}, {"$push": {"devices": dict(device)}}
+    )
+    return {"message": "successfully added new device <3"}
+
+
+@device.get("/energy-class")
+def get_all_possible_energy_classes():
+    return {member.name: member.value for member in EnergyClass}
+
+
+@device.get("/device-type")
+def get_all_device_classes():
+    return {it.name: it.value for it in DeviceType}
