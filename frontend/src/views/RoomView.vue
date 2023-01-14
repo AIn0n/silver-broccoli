37dfd39d25120c128bb9035128f912e5309fe1df
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-14 14:43:51 +0100

    Minor improvements in room view

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -10,18 +10,18 @@ const router = useRouter();
 const name = route.params.name;
 const devices = ref([]);
 
-onBeforeMount(() => {
-  api
-    .get("/" + name + "/device")
-    .then((res) => (devices.value = res.data))
-    .catch((e) => router.back());
-});
+api
+  .get("/" + name + "/device")
+  .then((res) => (devices.value = res.data))
+  .catch(router.back);
 </script>
 
 <template lang="pug">
 div(class="row container")
   BorderList(title="Devices")
-    li(@click="router.push('/edit-device/' + device.name)" class="list-group-item list-group-item-action fs-5" v-for="device in devices") {{ device.name }}
+    li(class="list-group-item list-group-item-action d-flex justify-content-between" v-for="device in devices")
+      span(class="fs-5" @click="router.push('/edit-device/' + device.name)") {{ device.name }}
+      button(type="button" class="btn-close" aria-label="Close")
     li(@click="router.push('/' + name + '/add-device')" class="list-group-item list-group-item-action list-group-item-primary fs-5") Add new device
   div(class="col text-center")
     h1(class="my-5") {{ name }}
